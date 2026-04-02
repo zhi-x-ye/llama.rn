63400e5b868f469336670bacdac45ae8c7721950
Zhi Ye <138628457+xyz-5@users.noreply.github.com>
2026-04-02 16:17:41 -0700

    chore: add built lib/ for GitHub dependency resolution

---
+++ lib/typescript/NativeRNLlama.d.ts
@@ -0,0 +1,7 @@
+import type { TurboModule } from 'react-native';
+export interface Spec extends TurboModule {
+    install(): Promise<boolean>;
+}
+declare const _default: Spec;
+export default _default;
+//# sourceMappingURL=NativeRNLlama.d.ts.map
---
+++ lib/typescript/version.d.ts
@@ -0,0 +1,3 @@
+export declare const BUILD_NUMBER = "8547";
+export declare const BUILD_COMMIT = "6861f65";
+//# sourceMappingURL=version.d.ts.map
